4730f565b4bfa4e3ca3299d9a834194fbd2a5e29
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-07-05 19:28:19 +0100

    prodcutrs component

--- src/pages/Productss/Productss.jsx
+++ src/pages/Productss/Productss.jsx
@@ -1,6 +1,62 @@
+import { useParams } from "react-router-dom";
+import List from "../../components/List/List";
+import { useState } from "react";
+import "./Productss.scss"
 
 const Productss = () => {
-  return <div>Productss</div>;
+
+  const catId = parseInt(useParams().id)
+
+  const [maxPrice, setMaxPrice] = useState(100000)
+  const [sort, setSort] = useState(null)
+
+  console.log(catId);
+  
+  return <div className="products">
+    <div className="left">
+      <div className="filterItem">
+        <h2>
+          Product Categories
+        </h2>
+        <div className="inputItem">
+          <input type="checkbox" className="" id="1" value={1} />
+          <label htmlFor="1" >Shirts</label>
+        </div>
+        <div className="inputItem">
+          <input type="checkbox" className="" id="2" value={2} />
+          <label htmlFor="2" >Shoes</label>
+        </div>
+        <div className="inputItem">
+          <input type="checkbox" className="" id="3" value={3} />
+          <label htmlFor="3" >Accessories</label>
+        </div>
+      </div>
+      <div className="filterItem">
+        <h2>Filter by Price</h2>
+        <div className="inputItem">
+          <span>0</span>
+          <input type="range"  min={0} max={100000} onChange={(e)=> setMaxPrice(e.target.value)} className="" />
+          <span>{maxPrice}</span>
+        </div>
+      </div>
+      <div className="filterItem">
+        <h2>Sort By</h2>
+        <div className="inputItem">
+          <input type="radio" id="asc" value={"asc"} name="price" onChange={()=> setSort("asc")} />
+          <label htmlFor="asc" className="label">Price(asc)</label>
+        </div>
+        <div className="inputItem">
+          <input type="radio" id="desc" value={"desc"} name="price" onChange={()=> setSort("desc")} />
+          <label htmlFor="desc" className="label">Price(desc)</label>
+        </div>
+      </div>
+    </div>
+    <div className="right">
+      <img src="https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+       alt="" className="catImage" />
+        <List sort={sort} catId={catId} maxPrice={maxPrice}/>
+    </div>
+  </div>;
 };
 
 export default Productss;
