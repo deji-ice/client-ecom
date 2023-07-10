a2b6b5ba7a2c265d3f20518e9df3cf59716175c5
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-07-10 16:59:23 +0100

    fetching strapi api

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,52 +1,31 @@
-
-import { useEffect, useState } from "react";
+import useFetch from "../../hooks/useFetch";
 import Card from "../Card/Card";
-import "./FeaturedProducts.scss"
-import axios from "axios";
-
+import "./FeaturedProducts.scss";
 
 // eslint-disable-next-line react/prop-types
 const FeaturedProducts = ({ type }) => {
+  const { data, loading, error } = useFetch(`/products?populate=*&[filters][type][$eq]=${type}`);
+  console.log(data);
 
-  const [products, setProducts] = useState([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await axios.get(import.meta.env.VITE_API_URL + `/products?populate=*&[filters][type][$eq]=${type}`, {
-    
-          headers: {
-            Authorization: "bearer " + import.meta.env.VITE_API_TOKEN,
-          },
-        })
-        setProducts(data?.data.data)
-        console.log(data?.data.data);
-        
-        console.log(products);
-        
-
-      } catch (error) {
-        console.log(error);
-
-      }
-    }
-    console.log(import.meta.env.VITE_API_TOKEN);
-    
-    fetchData()
-  }, [])
-
-  return <div className="featured">
-    <div className="top">
-      <h2>{type} Products</h2>
-      <p> Sed sagittis euismod tincidunt. Maecenas malesuada ipsum non lacus finibus ultrices.
-        Sed luctus metus in sapien scelerisque, </p>
+  return (
+    <div className="featured">
+      <div className="top">
+        <h2>{type} Products</h2>
+        <p>
+          Sed sagittis euismod tincidunt. Maecenas malesuada ipsum non lacus finibus ultrices. Sed luctus metus in sapien scelerisque.
+        </p>
+      </div>
+      <div className="bottom">
+        {error ? (
+          "Error!!!"
+        ) : loading ? (
+          "Loading..."
+        ) : data ? (
+          data.map((item) => <Card key={item.id} item={item} />)
+        ) : null}
+      </div>
     </div>
-    <div className="bottom">
-      {products.map((item) => (
-        <Card key={item.id} item={item} />
-      ))}
-    </div>
-  </div>;
+  );
 };
 
 export default FeaturedProducts;
