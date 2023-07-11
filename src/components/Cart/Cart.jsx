5f8efb077ab3a149a2a1ba8d91fe8e9c6c57f428
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-07-11 23:40:41 +0100

    integrated redux actiosn and disptach

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,49 +1,46 @@
 import { DeleteOutlineOutlined } from "@mui/icons-material";
 import "./Cart.scss"
+import { useDispatch, useSelector } from "react-redux";
+import { reduceItem, reset } from "../../redux/cartReducer";
 
 const Cart = () => {
-    const data = [{
-        id: 2,
-        img: 'https://images.unsplash.com/photo-1618109176213-554d5266923f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80',
-        img2: 'https://images.unsplash.com/photo-1618108730205-27d6ed54d012?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80',
-        title: 'Product 2',
-        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis euismod tincidunt.",
-        isNew: false,
-        oldPrice: 60,
-        price: 45,
-    }, {
-        id: 3,
-        img: 'https://images.unsplash.com/photo-1623596305214-19f21cbf48ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80',
-        img2: 'https://images.unsplash.com/photo-1629941199828-59f5ca36a985?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHN0cmVldHdlYXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
-        title: 'Product 3',
-        desc:"        Maecenas malesuada ipsum non lacus finibus ultrices. Sed luctus metus in sapien scelerisque,",
-        isNew: true,
-        oldPrice: 70,
-        price: 55,
-    },]
+
+    const products = useSelector(state => state.cart.products)
+    const dispatch = useDispatch()
+    const totalPrice=()=>{
+        let total = 0;
+        products.forEach(element => {
+            total += element.quantity * element.price
+        });
+
+        return total.toFixed(2)
+    }
 
     return <div className="cart">
         <h2>PRODUCTS IN CART</h2>
-        {data?.map((item) => (
+        {products?.map((item) => (
+            <>
             <div className="item" key={item.id}>
-                <img src={item.img} alt={item.title} />
+                <img src={import.meta.env.VITE_API_UPLOAD_URL +item.img} alt={item.title} />
                 <div className="details">
                     <h3>{item.title}</h3>
-                    <p>{item?.desc.substring(0, 80)}...</p>
+                    <p>{item?.description.substring(0, 80)}...</p>
                     <div className="price">
-                        1 x {item.price}
+                        {item.quantity} x {item.price}
                     </div>
                 </div>
-                
-                <DeleteOutlineOutlined className="delete" />
+
+                <DeleteOutlineOutlined className="delete" onClick={()=>dispatch(reduceItem(item.id))} />
             </div>
-        ))}
-    <div className="total">
-        <span>SUBTOTAL</span>
-        <span>&#x20A6;45,000</span>
-    </div>
-<button>PROCEED TO CHECKOUT</button>
-<span className="reset">Reset Cart</span>
+            </>
+        ))} 
+        <div className="total">
+            <span>SUBTOTAL</span>
+            <span>&#x20A6;{totalPrice()}</span>
+        </div>
+
+        <button>PROCEED TO CHECKOUT</button>
+        <span className="reset" onClick={()=>dispatch(reset())}>Reset Cart</span>
     </div>;
 };
 
--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,63 +1,74 @@
 import { useState } from "react";
 import "./Product.scss"
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { Balance,  FavoriteBorder } from "@mui/icons-material";
+import { Balance, FavoriteBorder } from "@mui/icons-material";
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/cartReducer";
 
 const Product = () => {
-
-  const [selectedImage, setSelectedImage] = useState(0)
+  const id = parseInt(useParams().id)
+  const [selectedImage, setSelectedImage] = useState("img")
   const [quantity, setQuantity] = useState(1)
-
-  const images = [
-    "https://images.unsplash.com/photo-1568253130222-5fc7d0173808?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1568251863885-adcac989de24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=397&q=80"
-  ]
+  const dispatch = useDispatch()
+  const { data, loading} = useFetch(
+    `/products/${id}?populate=*`)
 
   return <div className="product">
-    <div className="left">
+
+    {loading ? "loading..." : (<> <div className="left">
       <div className="images">
-        <img src={images[0]} alt="" onClick={() => setSelectedImage(0)} />
-        <img src={images[1]} alt="" onClick={() => setSelectedImage(1)} />
+        <img src={import.meta.env.VITE_API_UPLOAD_URL + data?.attributes.img?.data.attributes.url} alt="" onClick={() => setSelectedImage("img")} />
+        <img src={import.meta.env.VITE_API_UPLOAD_URL + data?.attributes.img2?.data.attributes.url} alt="" onClick={() => setSelectedImage("img2")} />
       </div>
       <div className="mainImage">
-        <img src={images[selectedImage]} alt="" />
+        <img src={import.meta.env.VITE_API_UPLOAD_URL + data?.attributes[selectedImage]?.data.attributes.url} alt="" />
       </div>
     </div>
-    <div className="right">
-      <h1>Title</h1>
-      <span className="price">&#x20A6;13,000</span>
-      <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis euismod tincidunt.
-        Maecenas malesuada ipsum non lacus finibus ultrices. Sed luctus metus in sapien scelerisque,
-      </p>
-      <div className="quantity">
-        <button onClick={() => setQuantity(prev => prev === 1 ? 1 : -1)}>-</button>
-        {quantity}
-        <button onClick={() => setQuantity(prev => prev + 1)}>+</button>
-      </div>
-      <button className="add">
-        <AddShoppingCartIcon /> ADD TO CART
-      </button>
-      <div className="link">
-        <div className="item">
-          <FavoriteBorder /> ADD TO WISHLIST
+      <div className="right">
+        <h1>{data?.attributes.title}</h1>
+        <span className="price">&#x20A6;{data?.attributes.price}</span>
+        <p>
+          {data?.attributes.description}
+        </p>
+        <div className="quantity">
+          <button onClick={() => setQuantity(prev => prev === 1 ? 1 : -1)}>-</button>
+          {quantity}
+          <button onClick={() => setQuantity(prev => prev + 1)}>+</button>
         </div>
-        <div className="item"><Balance /> ADD TO COMPARE</div>
-      </div>
-      <div className="info">
-        <span>Vendor: Gucci</span>
-        <span>Product Type: Shirt</span>
-        <span>Tag: T-shirt, Fashion, Men</span>
-      </div>
-      <hr />
-      <div className="info">
-        <span>DESCRIPTION:</span>
+        <button className="add" onClick={()=> dispatch(addToCart({
+          id: data?.id,
+          price: data?.attributes.price,
+          title: data?.attributes.title,
+          description: data?.attributes.description,
+          img: data?.attributes.img?.data.attributes.url,
+          quantity,
+        }))}>
+          <AddShoppingCartIcon /> ADD TO CART
+        </button>
+        <div className="link">
+          <div className="item">
+            <FavoriteBorder /> ADD TO WISHLIST
+          </div>
+          <div className="item"><Balance /> ADD TO COMPARE</div>
+        </div>
+        <div className="info">
+          <span>Vendor: Gucci</span>
+          <span>Product Type: Shirt</span>
+          <span>Tag: T-shirt, Fashion, Men</span>
+        </div>
         <hr />
-        <span>ADDITONLA INFORMATION</span>
-        <hr />
-        <span>FAQ</span>
+        <div className="info">
+          <span>DESCRIPTION:</span>
+          <hr />
+          <span>ADDITONLA INFORMATION</span>
+          <hr />
+          <span>FAQ</span>
+        </div>
       </div>
-    </div>
-
+    </>)
+    }
   </div>;
 };
 
